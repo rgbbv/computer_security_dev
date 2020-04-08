8f22e29d0d9ca76bdd1d3fc77443287f710ed392
Tomer Ittah <tomerittah@gmail.com>
2020-04-08 17:44:52 +0300

    credentials popover

--- client/public/content/components/ManagePasswords.js
+++ client/public/content/components/ManagePasswords.js
@@ -1,53 +1,112 @@
 /*global chrome*/
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
-    Button
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Popper,
+  Fade,
+  Paper,
 } from "@material-ui/core";
-import jq from 'jquery';
-import { PasswordListActionsConstants } from '../../../src/stores/PasswordList/Constants'
+import jq from "jquery";
+import { PasswordListActionsConstants } from "../../../src/stores/PasswordList/Constants";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 200,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 export default function Test(props) {
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [getCredentials, setGetCredentials] = useState(false);
-    const [credentials, setCredentials] = useState({});
+  const [password, setPassword] = useState("");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [getCredentials, setGetCredentials] = useState(false);
+  const [credentials, setCredentials] = useState({});
+  const [showOptions, setShowOptions] = useState(false);
+  const classes = useStyles();
 
-    if (!getCredentials)
-        props.port.postMessage({type: PasswordListActionsConstants.GET_CREDENTIALS, payload: {url: window.location.toString()}});
-
-    props.port.onMessage.addListener(function (msg) {
-        if (msg.type === PasswordListActionsConstants.GET_CREDENTIALS_SUCCESS) {
-            console.log(msg.payload.credentials);
-            setCredentials(msg.payload.credentials);
-            setGetCredentials(true);
-        } else if (msg.type === PasswordListActionsConstants.GET_CREDENTIALS_FAILURE) {
-            console.log('b');
-            setGetCredentials(true);
-        }
+  if (!getCredentials)
+    props.port.postMessage({
+      type: PasswordListActionsConstants.GET_CREDENTIALS,
+      payload: { url: window.location.toString() },
     });
 
-    jq("input:password").on('input', function(event) {
-        // console.log(event.target.value);
-        setPassword(event.target.value)
-    });
+  props.port.onMessage.addListener(function (msg) {
+    if (msg.type === PasswordListActionsConstants.GET_CREDENTIALS_SUCCESS) {
+      // console.log(msg.payload.credentials);
+      setCredentials(msg.payload.credentials);
+      setGetCredentials(true);
+    } else if (
+      msg.type === PasswordListActionsConstants.GET_CREDENTIALS_FAILURE
+    ) {
+      // console.log('b');
+      setGetCredentials(true);
+    }
+  });
 
-    jq("input[type=email]").change(function(event) {
-        // console.log("email: " + event.target.value);
-        setEmail(event.target.value);
-    });
+  jq("input:password").on("input", function (event) {
+    // console.log(event.target.value);
+    setPassword(event.target.value);
+  });
 
-    jq("input:text").change(function(event) {
-        // console.log("username: " + event.target.value);
-        setUsername(event.target.value);
-    });
+  jq("input:password").on("focusin", function (event) {
+    // Checking if we have the credentials for this site
+    if (Object.keys(credentials).length !== 0) {
+      setShowOptions(true);
+    }
+  });
 
+  // jq("input:password").on('focusout', function (event) {
+  //     setShowOptions(false);
+  // });
 
-    return (
-        <div>
-            <p>Username: {username}</p>
-            <p>Password: {password}</p>
-            <p>Email: {email}</p>
-        </div>
-    );
+  jq("input[type=email]").change(function (event) {
+    // console.log("email: " + event.target.value);
+    setEmail(event.target.value);
+  });
+
+  jq("input:text").change(function (event) {
+    // console.log("username: " + event.target.value);
+    setUsername(event.target.value);
+  });
+
+  return (
+    <Popper
+      open={showOptions}
+      anchorEl={anchorEl}
+      placement="bottom"
+      transition
+    >
+      {({ TransitionProps }) => (
+        <Fade {...TransitionProps} timeout={350}>
+          <Paper>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem button onClick={() => setShowOptions(false)}>
+                <ListItemIcon>
+                  <VpnKeyIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary={credentials.username}
+                  secondary={Array(credentials.password.length).fill("*")}
+                />
+              </ListItem>
+              <Divider />
+              <ListItem button onClick={() => setShowOptions(false)}>
+                <ListItemText primary="Manage passwords" />
+              </ListItem>
+            </List>
+          </Paper>
+        </Fade>
+      )}
+    </Popper>
+  );
 }
--- client/public/content/index.js
+++ client/public/content/index.js
@@ -2,12 +2,13 @@
 import React from "react";
 import {render} from "react-dom";
 import ManagePasswords from "./components/ManagePasswords";
+import jq from "jquery";
 
 const port = chrome.runtime.connect({ name: "client_port" });
 
 const anchor = document.createElement('div');
 anchor.id = 'rcr-anchor';
-
-document.body.insertBefore(anchor, document.body.childNodes[0]);
+jq("input:password").after(anchor);
+// document.body.insertBefore(anchor, document.body.childNodes[0]);
 render(
     <ManagePasswords port={port}/>, document.getElementById('rcr-anchor'));
